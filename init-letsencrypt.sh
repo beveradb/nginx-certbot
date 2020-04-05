b40136cd811c6f0d0f491afcde44757ed8fe995b
Andrew Beveridge <andrew@beveridge.uk>
2020-04-05 13:18:18 +0000

    Added config for AB lon server

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,11 +5,11 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.example.org)
+domains=(lon.beveradb.com)
 rsa_key_size=4096
-data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+data_path="/home/andrew/config/rutorrent-config/certbot"
+email="andrew@beveridge.uk" # Adding a valid address is strongly recommended
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -78,3 +78,8 @@ echo
 
 echo "### Reloading nginx ..."
 docker-compose exec nginx nginx -s reload
+echo 
+
+echo "### Stopping nginx so it can be run from rutorrent docker-compose stack instead ..."
+docker-compose down
+
